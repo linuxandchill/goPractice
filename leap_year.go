7b0e6d313ef77809332f483b5d3a7169f4781e49
rivercodi <rivercodi@gmail.com>
2017-06-04 12:05:01 -0700

    leap yr

--- leap_year.go
+++ leap_year.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+/*
+
+Given a year, report if it is a leap year.
+on every year that is evenly divisible by 4
+  except every year that is evenly divisible by 100
+	    unless the year is also evenly divisible by 400
+
+*/
+
 func isLeapYear(yr int) bool {
 	if (yr%4 == 0 && yr%100 != 0) || yr%400 == 0 {
 		return true
